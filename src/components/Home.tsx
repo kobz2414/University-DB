684aae28179ff4a214b3a3f4504413ee481a0fb2
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-02-10 02:48:23 +0800

    Working version

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Results } from "./Results"
 
 export const Home = () => {
+    const [country, setCountry] = useState("")
+    const [name, setName] = useState("")
+    const [propName, setPropName] = useState("")
+    const [propCountry, setPropCountry] = useState("")
+
+    const handleSubmit = (name: string, country: string) => {
+        setPropName(name)
+        setPropCountry(country)
+    }
+
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <div>
@@ -18,6 +29,7 @@ export const Home = () => {
             type="text"
             placeholder="Ex. Philippines"
             className="input input-bordered w-full max-w-xs text-xs"
+            onChange={(e) => setCountry(e.target.value)}
           />
         </div>
         <div className="form-control w-72 max-w-xs">
@@ -28,14 +40,15 @@ export const Home = () => {
             type="text"
             placeholder="Ex. Hardvard"
             className="input input-bordered w-full max-w-xs text-xs"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
       </div>
       <div>
-        <button className="btn btn-active px-10">Search</button>
+        <button className="btn btn-active px-10 mb-7" onClick={() => handleSubmit(name, country)}>Search</button>
       </div>
 
-      <Results />
+      <Results nameProp = {propName} countryProp = {propCountry} />
     </div>
   );
 };
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,54 @@
-export const Results = () => {
-    return(
-        <p></p>
-    )
+import { useEffect, useState } from "react";
+
+interface Data {
+  country?: String;
+  web_pages?: string;
+  "state-province"?: String;
+  name?: String;
 }
+
+export const Results = (props: { nameProp: String; countryProp: String;}) => {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `http://universities.hipolabs.com/search?name=${
+          props.nameProp || props.countryProp ? props.nameProp : " "
+        }&country=${
+          props.nameProp || props.countryProp ? props.countryProp : ""
+        }`
+      );
+      const result = await res.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, [props.nameProp, props.countryProp]);
+
+  if (!props.nameProp && !props.countryProp) {
+    return null;
+  }
+
+  return (
+    <>
+      {data.length !== 0 ? (
+        data.map((item, index) => (
+            <div
+              key={index}
+              tabIndex={index}
+              className="collapse collapse-arrow border border-base-300 bg-primary rounded-box my-2"
+            >
+              <div className="collapse-title text-xl font-medium">
+                {item.name}
+              </div>
+              <div className="collapse-content">
+                <p>{item.country}</p>
+                <a href={item.web_pages}>{item.web_pages}</a>
+              </div>
+            </div>
+          ))
+      ): <progress className="progress w-56 mt-20"></progress>}
+    </>
+  );
+};
