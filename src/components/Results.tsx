6b4d2be01257ed6b3e16561d7552afae92da435c
kobz2414 <62010035+kobz2414@users.noreply.github.com>
2023-03-26 18:11:07 +0800

    Fixed condition for results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -7,49 +7,77 @@ interface Data {
   name?: String;
 }
 
-export const Results = (props: { nameProp: string; countryProp: string;}) => {
+export const Results = (props: { nameProp: string; countryProp: string }) => {
   const [data, setData] = useState<Data[]>([]);
 
-  const filterAPI = (result: any, prop: string, filter: Data[], type: string) => {
+  const filterAPI = (
+    result: any,
+    prop: string,
+    filter: Data[],
+    type: string
+  ) => {
     result.forEach((university: any) => {
-      const query = type === "name" ? university.name.toLowerCase().includes(prop) : university.country.toLowerCase().includes(prop)
+      const query =
+        type === "name"
+          ? university.name.toLowerCase().includes(prop)
+          : university.country.toLowerCase().includes(prop);
 
-      if(query){
-        if(!filter.some(data => data.country === university.country && data.name === university.name)){
-          filter.push(university)
+      if (query) {
+        if (
+          !filter.some(
+            (data) =>
+              data.country === university.country &&
+              data.name === university.name
+          )
+        ) {
+          filter.push(university);
         }
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json")
+      const res = await fetch(
+        "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json"
+      );
       const result = await res.json();
-      let filter: Data[] = []
+      let filter: Data[] = [];
 
-
-      if(props.nameProp && props.countryProp){
+      if (props.nameProp || props.countryProp) {
         result.forEach((university: any) => {
-          if(university.name.toLowerCase().includes(props.nameProp.toLowerCase()) && university.country.toLowerCase().includes(props.countryProp.toLowerCase())){
-            if(!filter.some(data => data.country === university.country && data.name === university.name)){
-              filter.push(university)
+          if (
+            university.name
+              .toLowerCase()
+              .includes(props.nameProp.toLowerCase()) &&
+            university.country
+              .toLowerCase()
+              .includes(props.countryProp.toLowerCase())
+          ) {
+            if (
+              !filter.some(
+                (data) =>
+                  data.country === university.country &&
+                  data.name === university.name
+              )
+            ) {
+              filter.push(university);
             }
           }
-        })
-      }else{
-        if(props.nameProp){
-          filterAPI(result, props.nameProp, filter, "name")
+        });
+      } else {
+        if (props.nameProp) {
+          filterAPI(result, props.nameProp, filter, "name");
         }
-  
-        if(props.countryProp){
-          filterAPI(result, props.countryProp, filter, "country")
+
+        if (props.countryProp) {
+          filterAPI(result, props.countryProp, filter, "country");
         }
       }
 
-      filter.sort((a, b) =>{
-        return Number((b.name ?? "") > (a.name ?? ""))
-      })
+      filter.sort((a, b) => {
+        return Number((b.name ?? "") > (a.name ?? ""));
+      });
 
       setData(filter);
     };
@@ -65,21 +93,23 @@ export const Results = (props: { nameProp: string; countryProp: string;}) => {
     <>
       {data.length !== 0 ? (
         data.map((item, index) => (
-            <div
-              key={index}
-              tabIndex={index}
-              className="collapse collapse-arrow border border-base-300 bg-primary rounded-box my-2"
-            >
-              <div className="collapse-title text-xl font-medium">
-                {item.name}
-              </div>
-              <div className="collapse-content">
-                <p>{item.country}</p>
-                <a href={item.web_pages}>{item.web_pages}</a>
-              </div>
+          <div
+            key={index}
+            tabIndex={index}
+            className="collapse collapse-arrow border border-base-300 bg-primary rounded-box my-2"
+          >
+            <div className="collapse-title text-xl font-medium">
+              {item.name}
             </div>
-          ))
-      ): <progress className="progress w-56 mt-20"></progress>}
+            <div className="collapse-content">
+              <p>{item.country}</p>
+              <a href={item.web_pages}>{item.web_pages}</a>
+            </div>
+          </div>
+        ))
+      ) : (
+        <progress className="progress w-56 mt-20"></progress>
+      )}
     </>
   );
 };
