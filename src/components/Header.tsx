c80d71cb38ffc996afb5a525a1c7c0bbb6bf30d4
kobz2414 <62010035+kobz2414@users.noreply.github.com>
2023-03-26 18:10:02 +0800

    Renamed NavBar to Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const Header = () => {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <div className="navbar-start"></div>
+      <div className="navbar-center">
+        <a className="font-semibold text-xl">checkU</a>
+        <p className="text-xs ml-2">get details about your university</p>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+};
